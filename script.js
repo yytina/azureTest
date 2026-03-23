fb7dc64bf98d0b4b0aa1a3da21b24cc5ad8082c5
Hyejin Yang <yang.hyejin@gmail.com>
2026-03-23 11:41:24 +0900

    2026-03-23 3교시

---
+++ script.js
@@ -0,0 +1,10 @@
+// 버튼 클릭 시 실행되는 함수
+function changeMessage() {
+
+    // description 요소 가져오기
+    const description = document.getElementById("description");
+
+    // 텍스트 변경
+    description.innerText = "🎉 Azure 배포가 정상적으로 작동하고 있습니다!";
+
+}
